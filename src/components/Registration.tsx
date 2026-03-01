7c7b2cb8b4cb23d11102c364248db110d8db3484
Kaspalko <pal.kasparek@gmail.com>
2026-03-01 17:00:44 +0100

    Add background color to registration section
    Added a background color to the registration section.

--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -1,6 +1,6 @@
 export default function Registration() {
   return (
-    <section id="jelentkezes" className="py-20">
+    <<section id="jelentkezes" className="py-20 bg-amber-50">>
       <div className="container mx-auto px-4">
         <div className="max-w-4xl mx-auto">
           
