458c40001426f45f3cbdf8428ec6dd3bc8ac90b7
Kaspalko <pal.kasparek@gmail.com>
2026-03-10 20:55:15 +0100

    Update cikkek.ts

--- src/data/cikkek.ts
+++ src/data/cikkek.ts
@@ -1,1 +1,17 @@
+export const cikkek = [
+  {
+    slug: "korai-felismeres",
+    title: "A korai felismerés szerepe a gyermek fejlődésében",
+    description:
+      "Miért fontos a fejlődési eltérések korai felismerése és a szűrővizsgálatok szerepe.",
+    image: "/images/pexels-fotios-photos-5435599.jpg",
+  },
 
+  {
+    slug: "hason-fekves",
+    title: "Miért fontos a hason fekvés?",
+    description:
+      "A hason fekvés fontos szerepet játszik a baba mozgásfejlődésében.",
+    image: "/images/tummy-time.jpg",
+  },
+];
